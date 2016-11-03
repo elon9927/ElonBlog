adab07f62e0e8264a5244b4c276326946e60e80d
elon <elon@sinfotech.cn>
2016-11-03 17:19:41 +0800

    增加楼号和评论时间及样式调整

--- app/models/post.rb
+++ app/models/post.rb
@@ -48,4 +48,13 @@ class Post < ActiveRecord::Base
   def unpublish
     update(published: false, published_at: Time.now)
   end
+
+=begin
+  def self.search_by_title(title)
+    str = title.strip
+    str2 = '%'+str+'%'
+    where(["title like ? and published = ?", str2, true])
+  end
+=end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     get 'about' => 'pages#about', as: :about
     get 'contact' => 'pages#contact', as: :contact
     resources :posts, only: [:show, :index] do
+
       resources :comments
     end
   end
